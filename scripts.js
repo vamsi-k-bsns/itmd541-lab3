07a65acd8da72e841fa1f95b40a8d586d1dfe416
Vamsi Krishna <vamsi.k.bsns@gmail.com>
2024-04-03 00:07:48 -0500

    prototype1

--- scripts.js
+++ scripts.js
@@ -1,63 +1,19 @@
 
-//setting up variables
-var bill = document.getElementById("bill");
-var tipSlider = document.getElementById("tipSlider");
-var tipPercent = document.getElementById("tipPercent");
-var tipValue = document.getElementById("tipValue");
-var billTotal = document.getElementById("billTotal");
+//function to calculate total bill [runs when either bill input or tip slider are changed]
 
-var tip = 0;
-var cost = 0;
-var totalBill = 0;
+function calcTotal() {
+    var billInput = document.getElementById('billInput').value;
 
-//parsing the value from the slider into a number to display to the user
-tipPercent.value = tipSlider.value;
+    if (billInput.indexOf('.') !== -1)
+        billInput = parseFloat(parseInt(parseFloat(billInput)*100))/100;
+    
+    document.getElementById('billInput').value = billInput;
 
-//function to update the tip amount and total bill fields
-function updateValues(event){
+    var tipPercent = parseFloat(document.getElementById('tipSlider').value);    
 
-    tipPercent.value = parseInt(tipSlider.value);
-    totalBill = 0;
+    document.getElementById('tipValue').value = parseFloat((tipPercent*billInput)/100).toFixed(2);
+    document.getElementById('billTotal').value = parseFloat(billInput + ((tipPercent*billInput)/100)).toFixed(2);
 
-    tipRate = parseInt(tipPercent.value);
-    cost = parseFloat(bill.value).toFixed(2);
-
-    //tip is calculated as tip percentage divided by 100 times the bill
-    tipValue.value = (cost*(tipRate/100)).toFixed(2);
-
-    //adding tip to initial bill to get total bill54644.5.
-    totalBill = (parseFloat(cost) + parseFloat(tipValue.value)).toFixed(2);
-
-    //displaying total bill to the user
-    billTotal.value = totalBill;
+    
 
 }
-
-//function to restrict what kinds of input can be typed
-function formatInput(event) {
-    let input = event.target.value;
-    
-    //replace any non-numeric input with null
-    input = input.replace(/[^\d.]/g,'');
-
-
-    //finding if there's a decimal point in the input
-    var decimalIndex = input.indexOf('.');
-
-    //decimal is present
-    if(decimalIndex !== -1){
-
-        //only have two places after the decimal in the input
-        input = input.substring(0,decimalIndex) + "." + input.substring(decimalIndex+1,decimalIndex+3);
- 
-    }
-
-
-
-    //display formatted input to user
-    event.target.value = input;
-
-    //call function to update other fields
-    updateValues();
-  }
-
