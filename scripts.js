49d0d10ba1e69841b1f938ca0ef16d16afb88eef
Vamsi Krishna <vamsi.k.bsns@gmail.com>
2024-04-03 05:40:03 -0500

    initial upload with mobile site

--- scripts.js
+++ scripts.js
@@ -2,18 +2,26 @@
 //function to calculate total bill [runs when either bill input or tip slider are changed]
 
 function calcTotal() {
+
     var billInput = document.getElementById('billInput').value;
 
-    if (billInput.indexOf('.') !== -1)
+    if (billInput.indexOf('.') !== -1){
         billInput = parseFloat(parseInt(parseFloat(billInput)*100))/100;
-    
-    document.getElementById('billInput').value = billInput;
+        document.getElementById('billInput').value = billInput;
+    }
 
-    var tipPercent = parseFloat(document.getElementById('tipSlider').value);    
+    var tipPercent = parseFloat(document.getElementById('tipSlider').value);
+
+    document.getElementById('percentMeter').textContent = tipPercent + "%";
 
     document.getElementById('tipValue').value = parseFloat((tipPercent*billInput)/100).toFixed(2);
-    document.getElementById('billTotal').value = parseFloat(billInput + ((tipPercent*billInput)/100)).toFixed(2);
+    document.getElementById('billTotal').value = parseFloat(parseFloat(billInput) + ((tipPercent*parseFloat(billInput))/100)).toFixed(2);
 
-    
+}
+
+function buttonPress(buttonNum){
+
+    document.getElementById('tipSlider').value = buttonNum;
+    calcTotal();
 
 }
