5407dc46838775c7f03b0a68ebfb1721fbb31b05
Vamsi Krishna <vamsi.k.bsns@gmail.com>
2024-04-08 02:41:30 -0500

    added a seperate function for tip slider inputs to fix a bug on bill inputs

--- scripts.js
+++ scripts.js
@@ -16,17 +16,6 @@ function calcTotal() {
     //updating tip percent field
     document.getElementById('percentMeter').textContent = tipPercent + "%";
 
-    //some conditions to focus/blur the tip percentage buttons
-    if(tipPercent==10)
-        document.getElementById('ten').focus();
-    if(tipPercent==15)
-        document.getElementById('fifteen').focus();
-    if(tipPercent==20)
-        document.getElementById('twenty').focus();
-    if(tipPercent !== 10 && tipPercent !== 15 && tipPercent !== 20)
-        //select every button in the file and run button.blur() on them all
-        [...document.querySelectorAll('button')].forEach(button => button.blur());
-
     //updating tip value and total bill fields
     document.getElementById('tipValue').value = parseFloat((tipPercent*billInput)/100).toFixed(2);
     document.getElementById('billTotal').value = parseFloat(parseFloat(billInput) + ((tipPercent*parseFloat(billInput))/100)).toFixed(2);
@@ -38,5 +27,28 @@ function buttonPress(buttonNum){
 
     document.getElementById('tipSlider').value = buttonNum;
     calcTotal();
+
+}
+
+//function to update fields when tip slider is moved
+function tipSlide(){
+
+    //updating tip percent field
+    var tipPercent = parseFloat(document.getElementById('tipSlider').value);
+    document.getElementById('percentMeter').textContent = tipPercent + "%";
+
+    //some conditions to focus/blur the tip percentage buttons
+    if(tipPercent==10)
+        document.getElementById('ten').focus();
+    if(tipPercent==15)
+        document.getElementById('fifteen').focus();
+    if(tipPercent==20)
+        document.getElementById('twenty').focus();
+    if(tipPercent !== 10 && tipPercent !== 15 && tipPercent !== 20){
+        //select every button in the file and run button.blur() on them all
+        [...document.querySelectorAll('button')].forEach(button => button.blur());
+        calcTotal();
+    }
+    else calcTotal();
 
 }
